87e61cf56e0050c18b6b721c8551a4ea6d0b34e6
Patrick Pacurar <patrickpacurar@yahoo.com>
2021-09-13 20:26:51 +0300

    resolved merge problems

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js";
+import { getFirestore, doc, setDoc, getDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCcF5OuDn_KArUfWVXXndHCoKWo_ZwRdlA",
+  authDomain: "principal33-frontend-cms.firebaseapp.com",
+  projectId: "principal33-frontend-cms",
+  storageBucket: "principal33-frontend-cms.appspot.com",
+  messagingSenderId: "150929172953",
+  appId: "1:150929172953:web:2749b7d4e2133309d311c8",
+  measurementId: "G-083WQ0SSV8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+var employeesTable = document.getElementById("table-employees");
+
 window.onload = () =>{
 
     document.getElementById("add-employee-button").addEventListener("click", AddEmployee, false);
@@ -53,6 +77,7 @@ function AddEmployee() {
     validateForm = validate(lastName, firstName, email, gender, birthDate);
 
     if(validateForm) {
+
         employeeId = JSON.parse(localStorage.getItem('employeeNextId'));
         allEmployees =  JSON.parse(localStorage.getItem('employees'));
     
